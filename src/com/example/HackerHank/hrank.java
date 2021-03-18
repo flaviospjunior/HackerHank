adb7639a2d24bff943c53211acaf54bfc77345c2
flaviospjunior <flaviospjunior@gmail.com>
2021-03-18 01:30:57 -0300

    HackerHank

---
+++ src/com/example/HackerHank/hrank.java
@@ -0,0 +1,21 @@
+package com.example.HackerHank;
+
+import java.util.Scanner;
+
+
+public class hrank {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String x = scan.nextLine();
+        String s = scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
