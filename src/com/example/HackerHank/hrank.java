340b180ac948fa1f79d8368fc629b860daca6391
flaviospjunior <flaviospjunior@gmail.com>
2021-03-20 00:09:06 -0300

    Java Loops I

--- src/com/example/HackerHank/hrank.java
+++ src/com/example/HackerHank/hrank.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 
 public class hrank {
+
+
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        String x = scan.nextLine();
-        String s = scan.nextLine();
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        for (int i = 1; i <= 10; i++) {
+            int resultado;
+            resultado = i * N;
 
-        // Write your code here.
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+            System.out.println(N +  " x " + i + " = " + resultado);
+        }
     }
 }
